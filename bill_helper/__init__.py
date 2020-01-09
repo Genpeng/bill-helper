f6c477127b50d93275f49db5d1444be7da940f6f
Genpeng Xu <xgp1227@gmai.com>
2020-01-09 10:54:06 +0800

    version 0.0.1

--- bill_helper/__init__.py
+++ bill_helper/__init__.py
@@ -1,3 +1,3 @@
-from core.bill_classify import classify_bill
-from core.bill_similarity_search import find_k_nearest_bills
+from .bill_classify import classify_bill
+from .bill_similarity_search import find_k_nearest_bills
 __version__ = '0.0.1'
